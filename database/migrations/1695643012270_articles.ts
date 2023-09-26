64b13fb3fe57ae7364d962d21ff29172dafca957
Rasoulmadani <ali.rahbary@chmail.ir>
2023-09-26 08:58:57 +0330

    انجام مایگریشن در پایگاه داده

--- database/migrations/1695643012270_articles.ts
+++ database/migrations/1695643012270_articles.ts
@@ -6,7 +6,10 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
+      table.string('title')
+      table.string('slug').unique()
+      table.string("image") 
+      table.text('content')
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
